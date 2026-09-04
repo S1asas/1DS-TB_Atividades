d8e5b32d7757813970c64abea21b0ac54e4c25b3
S1asas <sasouza641@gmail.com>
2026-09-04 15:19:32 -0300

    Aula dia 04 do 9

--- api/models.py
+++ api/models.py
@@ -2,10 +2,8 @@ from django.db import models
 # Create your models here.
 class categoria (models.Model):
     nome = models.CharField(max_length=100)
-    descricao = models.CharField(max_length=255)
+    descricao = models.TextField()
     ativa = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.nome
-
-  
+        return self.nome 
